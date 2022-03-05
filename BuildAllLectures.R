b810ad834245c6915353772234e2b72a4ae4c044
Rodney <rjdyer@vcu.edu>
2022-03-05 14:51:40 -0500

    logistics

--- BuildAllLectures.R
+++ BuildAllLectures.R
@@ -81,6 +81,7 @@ system("find docs -iname '*.zip' -delete" )
 
 # clean up the lecture folder
 # system("find lectures -iname '*.html' -delete")
+system( "find lectures -name '*nb *.html' -delete")
 system( "find lectures -iname '.DS_Store' -delete")
 system( "find docs -iname '.DS_Store' -delete")
 system("find docs -iname '*.rda' -delete")
